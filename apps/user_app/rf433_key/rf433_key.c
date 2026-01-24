90ad9852d5cf04e11a55ca11a49d20863d8f7bd9
voidobserve <99012263+voidobserve@users.noreply.github.com>
2026-01-24 14:50:15 +0800

    2026-01-24-周六，CRC-1D84-9200AA2F
    测试发现换了其他的433模块后，由于433模块上面芯片的引脚拉低能力有限，需要单片机关闭上拉才能接收到信号。
    - 修改了433接收引脚的配置，关闭了原来的上拉
    
    功能之前就已经完成，最后得到的软件码是：【`1D84-9200AA2F`】

--- apps/user_app/rf433_key/rf433_key.c
+++ apps/user_app/rf433_key/rf433_key.c
@@ -13,7 +13,7 @@ volatile u8 flag_is_received_rf_433_data = 0; // 标志位，是否接收到了4
 
 void rf_433_key_config(void)
 {
-	gpio_set_pull_up(RF_433_KEY_SCAN_PIN, 1);	// 上拉
+	gpio_set_pull_up(RF_433_KEY_SCAN_PIN, 0);	// 不上拉
 	gpio_set_pull_down(RF_433_KEY_SCAN_PIN, 0); // 不下拉
 
 	gpio_set_die(RF_433_KEY_SCAN_PIN, 1);		// 普通输入模式
